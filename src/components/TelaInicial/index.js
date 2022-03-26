61f82d51488f5070873178b9fbb0e87aa44cdbd7
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-03-26 13:34:12 -0300

    obtendo lista de filmes da API

--- src/components/TelaInicial/index.js
+++ src/components/TelaInicial/index.js
@@ -1,37 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import ParagrafoTopo from "../ParagrafoTopo";
 
-import imgFilme from "./../../assets/img/img-filme.png";
 import "./style.css";
 
 function TelaInicial() {
+    const [listaFilmes, setListaFilmes] = useState([]);
+
+    function obterListaDeFilmes(){
+        axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+        .then((response)=>{
+            console.log(response.data);
+            setListaFilmes(response.data);
+        })
+        .catch((error)=>{
+            console.log(error.response);
+        })
+    }
+
+    useEffect(()=>{
+        obterListaDeFilmes();
+    }, [])
+
     return ( 
         <main className="TelaInicial">
             <ParagrafoTopo classe="topo-inicio" texto="Selecione o filme" />
             <section className="conteudo">
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
-                <figure>
-                    <img src={imgFilme} alt="imagem" />
-                </figure>
+                {
+                    listaFilmes.map((filme, index)=>{
+                    return (
+                        <figure key={index} id={filme.id} title={filme.title}>
+                            <img src={filme.posterURL} alt={filme.title} />
+                        </figure>
+                    )
+                    })
+                }
             </section>
         </main>
     );
